a9dc92a65e65167d36a4eeeaa04d5db5c2538b46
Huajie Zhong <hz642@cornell.edu>
2025-06-24 16:42:42 -0400

    update schema

--- src/db/schema/user.ts
+++ src/db/schema/user.ts
@@ -5,7 +5,8 @@ export const users = pgTable('users', {
   email: text('email').primaryKey(),
   name: text('name').notNull(),
   totalSurveysTaken: integer('total_surveys_taken').default(0).notNull(),
-  emailNotifications: boolean('email_notifications').default(true).notNull()
+  emailNotifications: boolean('email_notifications').default(true).notNull(),
+  hasRegistered: boolean('has_registered').default(false).notNull()
 }, (table) => [
   // Email validation constraint (Cornell emails only)
   check(
