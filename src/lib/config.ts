319dcbeff10baf95fc5525b01e3be7d597fee766
Huajie Zhong <hz642@cornell.edu>
2025-10-26 21:46:17 -0400

    update apiBaseUrl to prioritize NEXT_PUBLIC_API_URL in config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,5 @@
 export const config = {
-  apiBaseUrl: `${process.env.NEXTAUTH_URL || "http://localhost:3000"}/api`,
+  apiBaseUrl: `${
+    process.env.NEXT_PUBLIC_API_URL || process.env.NEXTAUTH_URL || "http://localhost:3000"
+  }/api`,
 } as const;
